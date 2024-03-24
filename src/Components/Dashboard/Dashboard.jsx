101e450dbd4cb26934fffd7f53c586203234fb5f
Josh-techie <youse2002@outlook.fr>
2024-03-24 01:58:22 +0000

    Added-Aside-Dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,14 +1,12 @@
-import { getAuth, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import Aside from "./Aside";
 
 const Dashboard = () => {
-  const auth = getAuth();
-  const navigate = useNavigate();
   const authUser = getAuth().currentUser;
 
   if (!authUser) {
     return (
-      <div className="margin-auto">
+      <div className="margin-auto justify-center">
         <img
           src={
             "https://user-images.githubusercontent.com/47600906/92342363-50f71f00-f0de-11ea-85cf-d0af41acc6c8.png"
@@ -18,25 +16,11 @@ const Dashboard = () => {
       </div>
     );
   }
-  const userSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        navigate("/login"); // Redirect to the sign-in page after signing out
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+
 
   return (
     <div>
-      {/* Styled sign-out button */}
-      <button
-        onClick={userSignOut}
-        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mt-4 ml-4"
-      >
-        Sign Out
-      </button>
+      <Aside />
     </div>
   );
 };
